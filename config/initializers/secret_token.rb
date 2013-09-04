a968bd5d31fefa07bb4fff0e4b04e0423a25937b
Donny Kurnia <donnykurnia@gmail.com>
2013-09-04 08:37:19 +0700

    update config for rails 4.0
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
 JqueryFileuploadRailsExample::Application.config.secret_token = '53799ea9a3d06ff292dc489c308943155ef18f4a6aaab7176c4b8fcfc123294e3ff69df9327c65d0c0021efe3feb8b57ce8c233802916f76ee13737fda04983c'
