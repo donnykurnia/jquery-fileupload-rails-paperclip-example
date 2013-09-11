e4fa814b079b972ee66215168ad55eff66ce49ce
Donny Kurnia <donnykurnia@gmail.com>
2013-09-11 13:18:12 +0700

    make nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 JqueryFileuploadRailsExample::Application.routes.draw do
 
-  resources :uploads
-  resources :prompts
+  resources :prompts do
+    resources :uploads
+  end
   root 'prompts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
